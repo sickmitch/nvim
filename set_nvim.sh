0d4610048d0b10e21401bc00d88b8633b1e2f975
sickmitch <marinic.michele@proton.me>
2023-08-27 18:02:28 +0200

    typo

--- set_nvim.sh
+++ set_nvim.sh
@@ -7,6 +7,7 @@ D=~/.config/nvim/
 mkdir $D
 cp init.lua $D
 cp -r lua/ $D
+cp -r spell/ $D
 
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
